6e02f650cbe1730b6dfe8618992853b931baba36
unknown <yjh984@daum.net>
2021-01-03 17:07:58 +0900

    v0.8 : almost done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movies from './Movies';
+import './App.css';
 
 let searchYear = new Date().getFullYear();
 
@@ -8,6 +9,7 @@ class App extends React.Component{
   state ={
     isLoading: true,
     isMovie: false,
+    isYear: true,
     moviesList: []
   }
   
@@ -29,49 +31,69 @@ class App extends React.Component{
   }
 
   handleBtnMinusYear=()=>{
+    this.setState({isLoading:true});
     searchYear = searchYear-1;
     this.getMoviesList();
   }
 
   handleBtnAddYear=()=>{
+    this.setState({isLoading:true});
     searchYear = searchYear+1;
     this.getMoviesList();
   }
 
   handleBtnAllList=()=>{
+    this.setState({isLoading:true, isYear:false});
     searchYear = "";
     this.getMoviesList();
   }
 
   handleBtnBackToYear=()=>{
+    this.setState({isLoading:true, isYear:true});
     searchYear = new Date().getFullYear();
     this.getMoviesList();
   }
 
   render(){
-    const {isLoading, isMovie, moviesList} = this.state;
+    const {isLoading, isYear, isMovie, moviesList} = this.state;
     return (
-      <div>
-        {isLoading? '' : (<h1>{searchYear} year.</h1>)}
-        {isLoading ? 'Please, wait for loading...' : 
-          (isMovie ? moviesList.map(item =>{
-              // console.log(item.title);
-                          return <Movies key={item.id}
-                                        id={item.id}
-                                        year={item.year}
-                                        title={item.title}
-                                        genres={item.genres}
-                                        summary={item.summary}
-                                        poster={item.medium_cover_image} />}) :
-                     <h4>There is no movie which you want..</h4>)
-        }
-        <div>
-        {isLoading? '' : (<button onClick={this.handleBtnMinusYear}>Previous Year</button>)}
-        {isLoading? '' : (<button onClick={this.handleBtnAddYear}>Next Year</button>)}
+      <section className='container'>
+        <div className='front'>
+          {isLoading? '' : 
+            isYear? (<h1>YEAR {searchYear}.</h1>) : <h1>All YEAR.</h1>}
+          <div>
+          {isLoading? '' : 
+            isYear? (<button onClick={this.handleBtnMinusYear}>Previous Year</button>) : ""}
+          {isLoading? '' : 
+            isYear? (<button onClick={this.handleBtnAddYear}>Next Year</button>) : ""}
+          </div>
+          <div>
+          {isLoading? '' : (<button onClick={this.handleBtnAllList}>All Movies</button>)}
+          {isLoading? '' : (<button onClick={this.handleBtnBackToYear}>Back to this year</button>)}
+          </div>
         </div>
-        {isLoading? '' : (<button onClick={this.handleBtnAllList}>All Movies</button>)}
-        {isLoading? '' : (<button onClick={this.handleBtnBackToYear}>Back to this year</button>)}
-      </div>
+
+        {isLoading ? 
+          <div className='loader'>
+            <span className='loader__text'>'Please, wait for loading...' </span>
+          </div>: 
+          (isMovie ? 
+            (<div className='movies'>
+            {moviesList.map(item =>{
+            // console.log(item.title);
+                        return <Movies key={item.id}
+                                      id={item.id}
+                                      year={item.year}
+                                      rating={parseFloat(item.rating).toFixed(1)}
+                                      title={item.title}
+                                      genres={item.genres}
+                                      summary={item.summary}
+                                      poster={item.medium_cover_image} />})}
+            </div>):
+            <h4>There is no movie which you want..</h4>
+          )
+        }
+      </section>
     )
   }
 };
--- src/Movies.js
+++ src/Movies.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movies.css';
 
-function Movies({id, year, title, genres, summary, poster}){
-    return (<div>
-                <h4>{title}</h4>
+function Movies({year, rating, title, genres, summary, poster}){
+    // let genresSlash='';
+    // genres.forEach(item=>{
+    //     genresSlash = genresSlash+item + '/';
+    // })
+    // console.log(genresSlas.substr(genresSlash));
+    // genresSlash=genresSlash.slice(0,genresSlash.length-1);
+
+    return (<div className='movie'>
+                <img src={poster} alt={title} title={title}/>
+                <div className='movie__data'>
+                    <h2 className='movie__title'>{title}</h2>
+                    <h4 className='movie__year'>{year} / {rating}</h4>
+                    <ul className='movie__genres'>
+                        {genres.map((item, index)=> (
+                            <li key={index} className='genres__genre'>{item}</li> )
+                        ) }    
+                    </ul>
+                    {/* <h4 className='movie__genres'>{genresSlash}</h4> */}
+                    <p className='movie__summary'>{summary.slice(0,300)}...</p>
+                </div>
             </div>
             );
 }
@@ -11,8 +30,9 @@ function Movies({id, year, title, genres, summary, poster}){
 Movies.prototype={
     id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
+    rating: PropTypes.number,
     title: PropTypes.string.isRequired,
-    genres: PropTypes.string,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired
 };
